1caf134959d1d6b4d3e390d0d4408d7a078568e1
leowrites <leo@liuwork.ca>
2026-03-10 14:01:41 -0400

    linter

--- src/core/database/__tests__/utils.test.ts
+++ src/core/database/__tests__/utils.test.ts
@@ -8,7 +8,9 @@ describe('generateId', () => {
   it('has the correct UUID-like format (8-4-4-4-12)', () => {
     const id = generateId();
     // UUID format: xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx
-    expect(id).toMatch(/^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/);
+    expect(id).toMatch(
+      /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/,
+    );
   });
 
   it('generates unique IDs on each call', () => {
